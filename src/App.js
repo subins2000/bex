374ac42fa58436d824cf9b4623d1088d813edec8
Subin Siby <subins2000@gmail.com>
2018-05-03 12:42:50 +0530

    Change indent to 4 spaces

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import Header from './Header.js';
 import './Home.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Header/>
+                    <p className="App-intro">
+                    </p>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;

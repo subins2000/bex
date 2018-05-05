a0ffaab241b16507af535046f2b1f432836eea4b
Subin Siby <subins2000@gmail.com>
2018-05-05 07:44:21 +0530

    Fix indentation to 4 spaces

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() {
-    return (
-      <header className="App-header">
-        <h1 className="App-title">TXTBook</h1>
-      </header>
-    );
-  }
+    render() {
+        return (
+            <header className="App-header">
+                <h1 className="App-title">BeX</h1>
+            </header>
+        );
+    }
 }
 
-export default Header
+export default Header;

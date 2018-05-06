cfe48edef60b48b0592efd2698b4cd759df44dda
Subin Siby <subins2000@gmail.com>
2018-05-06 22:32:38 +0530

    Separated components into a folder

---
+++ src/components/partial/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+    Link
+} from 'react-router-dom'
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">BeX</Link>
+                <form className="form-inline my-2 my-lg-0">
+                    <Link className="btn btn-light" to="/login">Login</Link>&nbsp;
+                    <Link className="btn btn-light" to="/register">Register</Link>
+                </form>
+            </nav>
+        );
+    }
+}
+
+export default Header;

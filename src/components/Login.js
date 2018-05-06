d725a2c208f255017511373e3973a21e5692a8b1
Subin Siby <subins2000@gmail.com>
2018-05-06 23:15:28 +0530

    Added login page

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Header from './partial/Header.js';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="container">
+                    <p>
+                        <h1>Login</h1>
+                    </p>
+                    <form>
+                        <div className="row">
+                            <div className="col">
+                                <input type="email" className="form-control" placeholder="Enter email" />
+                            </div>
+                            <div className="col">
+                                <input type="password" className="form-control" placeholder="Enter password" />
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;

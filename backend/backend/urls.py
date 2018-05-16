e06eddc3481c19f2866e50b849241abff2461641
Subin Siby <subins2000@gmail.com>
2018-05-17 00:08:37 +0530

    Route everything to index.html

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name="index.html")),
 ]

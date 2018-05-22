52f89f1800171747e6db0f127e78de990cb379d8
Subin Siby <subins2000@gmail.com>
2018-05-22 22:57:02 +0530

    Tidied up

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import {
+    Link
+} from 'react-router-dom'
 import Header from './partial/Header.js';
-
-import './css/Home.css';
 
 class Home extends Component {
     render() {
@@ -9,11 +10,14 @@ class Home extends Component {
             <div>
                 <Header/>
                 <div className="container" id="content">
-                    <div className="intro row align-items-center">
+                    <div className="row align-items-center">
                         <div className="col">
                             <center>
                                 <h1>404</h1>
                                 <p>This page doesn't exist.</p>
+                                <p>
+                                    <Link className="btn btn-warning" to="/">Home</Link>
+                                </p>
                             </center>
                         </div>
                     </div>

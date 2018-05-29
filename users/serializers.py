7b3198bbf34f25f556a24a9ec3dd927afbdec9ad
Subin Siby <subins2000@gmail.com>
2018-05-29 15:28:08 +0530

    Make password write only and use override create()

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,21 @@ class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('username', 'password', 'email', 'name')
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            },
+        }
+
+    def create(self, validated_data):
+        user = Users.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            name=validated_data['name'],
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user

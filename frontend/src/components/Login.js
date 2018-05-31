c38a2c93c8524f7a1dcd1db1217988eeed757d02
Subin Siby <subins2000@gmail.com>
2018-05-31 23:59:35 +0530

    Added input change handle and form submit handler

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import Header from './partial/Header.js';
 
 class Login extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            inputEmail: '',
+            inputPassword: '',
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+
+        var $this = this;
+    }
+
     render() {
         return (
             <div>
@@ -10,10 +32,10 @@ class Login extends Component {
                     <h1>Login</h1>
                     <form>
                         <div className="form-group">
-                            <input type="email" className="form-control" placeholder="Enter email" />
+                            <input type="email" className="form-control" placeholder="Enter email" value={this.state.inputEmail} onChange={this.handleInputChange} />
                         </div>
                         <div className="form-group">
-                            <input type="password" className="form-control" placeholder="Enter password" />
+                            <input type="password" className="form-control" placeholder="Enter password" value={this.state.inputPassword} onChange={this.handleInputChange} />
                         </div>
                         <button className="btn btn-primary">Login</button>
                     </form>

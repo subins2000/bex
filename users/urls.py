5743c34e41a33d01dc75f7164f944aba309adaa7
Subin Siby <subins2000@gmail.com>
2018-06-16 00:43:03 +0530

    Add login endpoint

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('api/users/register', views.UsersRegister.as_view()),
+    path('api/users/login', views.UsersLogin.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.models import Users
 from users.serializers import UsersSerializer
@@ -9,3 +13,17 @@ class UsersRegister(CreateAPIView):
 
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
+
+
+class UsersLogin(APIView):
+
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+
+        return Response(content)

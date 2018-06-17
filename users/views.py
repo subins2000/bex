994db76afa4820df0c6983b7141a7a1b115f00d7
Subin Siby <subins2000@gmail.com>
2018-06-17 23:37:32 +0530

    Use TokenAuthentication

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -17,13 +18,14 @@ class UsersRegister(CreateAPIView):
 
 class UsersLogin(APIView):
 
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (TokenAuthentication, BasicAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, format=None):
+        token, created = Token.objects.get_or_create(user=request.user)
+
         content = {
-            'user': str(request.user),  # `django.contrib.auth.User` instance.
-            'auth': str(request.auth),  # None
+            'token': str(token),
         }
 
         return Response(content)

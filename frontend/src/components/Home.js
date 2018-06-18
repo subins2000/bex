306a677b51b9f69e4ad6f8ef1458a2d4ef7fb6b6
Subin Siby <subins2000@gmail.com>
2018-06-18 15:23:14 +0530

    Separate UI for guest and logged in users

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
+
 import Header from './partial/Header.js';
+import {isLoggedIn} from '../App.js'
 
 import './css/Home.css';
 
 
 class Home extends Component {
+    intro() {
+        return (
+            <div className="container" id="content">
+                <div className="intro row align-items-center">
+                    <div className="col">
+                        <center>
+                            <h1>BeX</h1>
+                            <p>Easily exchange books !</p>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    dashboard() {
+        return (
+            <div className="container container-fluid" id="content">
+
+            </div>
+        );
+    }
+
     render() {
+        var content;
+
+        if (isLoggedIn()) {
+            content = this.dashboard();
+        } else {
+            content = this.intro();
+        }
+
         return (
             <div>
                 <Header/>
-                <div className="container" id="content">
-                    <div className="intro row align-items-center">
-                        <div className="col">
-                            <center>
-                                <h1>BeX</h1>
-                                <p>Easily exchange books !</p>
-                            </center>
-                        </div>
-                    </div>
-                </div>
+                {content}
             </div>
         );
     }

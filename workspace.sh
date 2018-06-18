09b378dbec4709071d01ccec2d049bc6d50b8352
Subin Siby <subins2000@gmail.com>
2018-06-18 14:52:17 +0530

    Actiavte venv in 3rd shell

--- workspace.sh
+++ workspace.sh
@@ -7,4 +7,4 @@ thunar $projectDir &
 xfce4-terminal -H \
     -T backend --working-directory=$projectDir -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; source bin/activate; python3 manage.py runserver;" bash' \
     --tab -T frontend --working-directory=$projectDir -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; source bin/activate; cd frontend && node_modules/.bin/webpack --config config/webpack.config.dev.js --watch;" bash' \
-    --tab -T git --working-directory=$projectDir
+    --tab -T git --working-directory=$projectDir -e 'env PROMPT_COMMAND="unset PROMPT_COMMAND; source bin/activate;" bash'

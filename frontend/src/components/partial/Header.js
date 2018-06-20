a573dc20e371ba4df3690998cd984722087283e1
Subin Siby <subins2000@gmail.com>
2018-06-20 23:34:42 +0530

    Different header for logged in and guests

--- frontend/src/components/partial/Header.js
+++ frontend/src/components/partial/Header.js
@@ -3,15 +3,42 @@ import {
     Link
 } from 'react-router-dom'
 
+import {isLoggedIn} from '../../App.js';
+
 
 class Header extends Component {
+    userButtons() {
+        return (
+            <div class="dropdown">
+                <a className="btn btn-primary"></a>&nbsp;
+                <a className="btn btn-light" to="/register">Log Out</a>
+            </div>
+        );
+    }
+
+    guestUserButtons() {
+        return (
+            <span>
+                <Link className="btn btn-light" to="/login">Log In</Link>&nbsp;
+                <Link className="btn btn-light" to="/register">Register</Link>
+            </span>
+        );
+    }
+
     render() {
+        var userControlButtons;
+
+        if (isLoggedIn()) {
+            userControlButtons = this.userButtons();
+        } else {
+            userControlButtons = this.guestUserButtons();
+        }
+
         return (
             <nav className="navbar navbar-dark bg-dark">
                 <Link className="navbar-brand" to="/">BeX</Link>
                 <form className="form-inline my-2 my-lg-0">
-                    <Link className="btn btn-light" to="/login">Login</Link>&nbsp;
-                    <Link className="btn btn-light" to="/register">Register</Link>
+                    {userControlButtons}
                 </form>
             </nav>
         );

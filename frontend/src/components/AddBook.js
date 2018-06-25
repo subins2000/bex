153d43d587ff71b921b7cd4741ccac664ba3aa9b
Subin Siby <subins2000@gmail.com>
2018-06-26 01:46:09 +0530

    Add form handlers

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -8,14 +8,41 @@ import Header from './partial/Header.js';
 
 
 class AddBook extends Component {
-    render() {
+    constructor(props){
+        super(props);
+
+        console.log(isLoggedIn());
+
         if (!isLoggedIn()) {
             this.props.history.push('/login');
         }
 
+        this.state = {};
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+
+        var $this = this;
+    }
+
+    render() {
         return (
             <div>
                 <Header/>
+                <div className="container" id="content">
+                    <h1>Add Book</h1>
+                    <form onSubmit={this.handleFormSubmit}>
+                        <button className="btn btn-primary">Add Book</button>
+                    </form>
+                </div>
             </div>
         );
     }

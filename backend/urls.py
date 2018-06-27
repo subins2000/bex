e8f33fabdb5984b2cf1101c9a4ea77d5605ccb89
Subin Siby <subins2000@gmail.com>
2018-06-27 23:24:11 +0530

    Add books.urls

--- backend/urls.py
+++ backend/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
+    url(r'', include('books.urls')),
     url(r'', include('users.urls')),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]

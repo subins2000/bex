7132f66aef7ba6ee342e6c20e3a197e2707e8410
Subin Siby <subins2000@gmail.com>
2018-06-28 00:36:31 +0530

    Add books serializer

---
+++ books/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from books.models import Books
+
+
+class BooksSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Books
+        fields = ('title', 'author', 'semester', 'branch', 'description')

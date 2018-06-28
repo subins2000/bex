c38ef5e82b1385d9bbb50bb3bda3caa8d3d30461
Subin Siby <subins2000@gmail.com>
2018-06-29 01:44:06 +0530

    Add anonymous user book add test

--- books/tests.py
+++ books/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase, APIClient
 
+from .models import Books
 from .views import BookAdd
 
 
@@ -16,6 +17,21 @@ class BookAddTest(APITestCase):
             password='password',
         )
         self.authToken = 'Token ' + str(Token.objects.create(user=self.user))
+
+    def testAnonymousUserAdd(self):
+        response = self.client.post(
+            '/api/books/add',
+            data={
+                'title': 'Calculus',
+                'author': 'Ben',
+                'semester': '1',
+                'branch': 'cse',
+                'description': 'Textbook for KTU',
+            }
+        )
+
+        # 401 Forbidden
+        self.assertEqual(response.status_code, 401)
 
     def testUserAdd(self):
         response = self.client.post(

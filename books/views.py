4aee1a79afc41dfc1b83c45227e87bb6930c5165
Subin Siby <subins2000@gmail.com>
2018-06-28 23:29:29 +0530

    Add auth class and allow request only for logged in users

--- books/views.py
+++ books/views.py
@@ -1,8 +1,15 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from books.models import Books
+from books.serializers import BooksSerializer
 
 
 class BookAdd(CreateAPIView):
 
     queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)

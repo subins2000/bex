3cf029767b4e5c865b71f301935a37b177b0a08a
Subin Siby <subins2000@gmail.com>
2018-07-07 23:47:43 +0530

    Get username from path or use username if logged in

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -2,13 +2,19 @@ import axios from 'axios';
 import React, { Component } from 'react';
 import toastr from 'toastr';
 
-import { isLoggedIn } from '../store.js';
+import { isLoggedIn, userStore } from '../store.js';
 import Header from './partial/Header.js';
 
 
 class Profile extends Component {
     constructor(props){
         super(props);
+
+        if (this.props.match.params.username) {
+            this.username = this.props.match.params.username;
+        } else if (isLoggedIn()) {
+            this.username = userStore.getState()['username'];
+        }
     }
 
     render() {

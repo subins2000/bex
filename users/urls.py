29bf0d93005adf09259b4568d3031378b7fbb8ea
Subin Siby <subins2000@gmail.com>
2018-07-08 23:19:18 +0530

    Get info from /api/users/info

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('api/users/register', views.UsersRegister.as_view()),
-    path('api/users/login', views.UsersLogin.as_view()),
     path('api/users/authcheck', views.AuthCheck.as_view()),
+    path('api/users/info', views.UserView.as_view()),
+    path('api/users/login', views.UsersLogin.as_view()),
+    path('api/users/register', views.UsersRegister.as_view()),
 ]

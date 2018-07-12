01b5600dba3f7f9a44030cbe1fc780c12723dd45
Subin Siby <subins2000@gmail.com>
2018-07-12 23:45:03 +0530

    Add a slug field for book

--- books/models.py
+++ books/models.py
@@ -1,23 +1,28 @@
 from django.db import models
+from django.utils.crypto import get_random_string
 
 from users.models import User
 
 
 class Books(models.Model):
 
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        db_column='user',
-    )
-    title = models.CharField(max_length=200)
     author = models.CharField(max_length=60)
-    semester = models.IntegerField()
     branch = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=500)
     photo = models.ImageField(
         blank=True,
         null=True,
         upload_to='books/%Y/%m/%d/'
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    semester = models.IntegerField()
+    slug = models.CharField(
+        max_length=12,
+        default=get_random_string(12)
+    )
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_column='user',
+    )

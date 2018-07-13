53d93c649f30c9a266f2161d79764433a0be701e
Subin Siby <subins2000@gmail.com>
2018-07-14 02:16:12 +0530

    Display book details

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -31,18 +31,69 @@ class Book extends Component {
         var $this = this;
 
         axios.get('/api/book/' + this.slug).then(function(r) {
-
+            $this.setState({
+                author: r.data.author,
+                branch: r.data.branch,
+                created_at: r.data.created_at,
+                description: r.data.description,
+                photo: r.data.photo,
+                semester: r.data.semester,
+                title: r.data.title,
+            });
         }).catch(function(e) {
 
         });
     }
 
     render() {
+        var branch = this.state.branch,
+            photo,
+            semester = this.state.semester;
+
+        if (this.state.photo) {
+            photo = (
+                <img src={this.state.photo} className="img-fluid" />
+            );
+        }
+
+        if (!semester) {
+            semester = (
+                <span className="alert alert-info">Not applicable</span>
+            );
+        }
+
+        if (!branch) {
+            branch = (
+                <span className="alert alert-info">Not applicable</span>
+            );
+        }
+
         return this.state.isNotFound ? <NotFound /> : (
             <div>
                 <Header/>
                 <div className="container" id="content">
-                    <h2>{this.state.title}</h2>
+                    <h1>{this.state.title}</h1>
+                    <div className="row card-body">
+                        <div className="col-3">
+                            {photo}
+                        </div>
+                        <div className="col-9">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">Author</th>
+                                        <td>{this.state.author}</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">Semester</th>
+                                        <td>{semester}</td>
+                                        <th scope="row">Branch</th>
+                                        <td>{branch}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         );

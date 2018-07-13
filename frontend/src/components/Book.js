1636a106e64a78a6979350c3bf5dcff46b330997
Subin Siby <subins2000@gmail.com>
2018-07-14 02:23:18 +0530

    No need of login to view books

--- frontend/src/components/Book.js
+++ frontend/src/components/Book.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 
-import { isLoggedIn, userStore } from '../store.js';
 import Header from './partial/Header.js';
 import NotFound from './NotFound.js';
 

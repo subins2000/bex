4eb566df0717609895644dd07c279962f113a17a
Subin Siby <subins2000@gmail.com>
2018-07-25 16:58:17 +0530

    Add a form to request book

---
+++ frontend/src/components/RequestBookForm.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+
+
+class RequestBookForm extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
+
+export default RequestBookForm;

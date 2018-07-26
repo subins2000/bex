b865401b08da59d2dc6251f638984261a6002738
Subin Siby <subins2000@gmail.com>
2018-07-26 23:38:57 +0530

    Added a msg txtbox

--- frontend/src/components/RequestBookForm.js
+++ frontend/src/components/RequestBookForm.js
@@ -6,10 +6,25 @@ class RequestBookForm extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            inputMessage: '',
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.id]: e.target.value});
     }
 
     render() {
-
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="inputMessage">Message</label>
+                    <textarea type="text" className="form-control" id="inputMessage" value={this.state.inputMessage} onChange={this.handleInputChange} />
+                </div>
+            </form>
+        )
     }
 }
 
